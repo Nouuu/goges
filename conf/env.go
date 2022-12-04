6315543e8bed28d8d691496d673d39170a497bee
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-12-04 18:46:46 +0100

    feat(google-api): Update env variables

--- conf/env.go
+++ conf/env.go
@@ -6,6 +6,8 @@ import (
 
 const USERNAME_ENV = "username"
 const PASSWORD_ENV = "password"
+const CalendarIdEnv = "CALENDAR_ID"
+const PlanningDaysSyncEnv = "PLANNING_DAYS_SYNC"
 
 func LoadEnv() {
 	godotenv.Load(".env")
