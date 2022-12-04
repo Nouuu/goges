0d38da26a047347ff2a36db8d1a15b7d48e29f7a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-12-04 18:51:51 +0100

    feat(google-api): print next 30 days

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"goges/conf"
-	"goges/kordis"
+	"goges/google_api"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
 	conf.LoadEnv()
 
-	credentials := kordis.GetMygesCredentials()
+	daysToSync, err := strconv.Atoi(os.Getenv(conf.PlanningDaysSyncEnv))
+	if err != nil {
+		panic(err)
+	}
 
-	kordis.Connect(&credentials)
+	if daysToSync <= 0 {
+		panic("PLANNING_DAYS_SYNC must be a positive integer")
+	}
+
+	googleCalendarClient, err := google_api.CalendarClientService()
+	if err != nil {
+		panic(err)
+	}
+	events, err := googleCalendarClient.GetEvents(time.Now(), time.Now().AddDate(0, 0, daysToSync))
+	if err != nil {
+		panic(err)
+	}
+	google_api.PrintEvents(events)
 }
