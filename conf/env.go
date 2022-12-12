d99ddbc644533d7c0e700efa4d7fb24cbd596e6f
Nouuu <nlarrieulacoste@myges.fr>
2022-12-12 16:13:36 +0100

    feat(env): Change env retrieving method

--- conf/env.go
+++ conf/env.go
@@ -1,19 +1,50 @@
 package conf
 
 import (
+	"fmt"
+	"github.com/caarlos0/env/v6"
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 )
 
-const UsernameEnv = "USERNAME"
-const PasswordEnv = "PASSWORD"
-const SchedulerCronEnv = "SCHEDULER_CRON"
-const CalendarIdEnv = "CALENDAR_ID"
-const PlanningDaysSyncEnv = "PLANNING_DAYS_SYNC"
+type Config struct {
+	Username         string `env:"USERNAME"`
+	Password         string `env:"PASSWORD"`
+	SchedulerCron    string `env:"SCHEDULER_CRON"`
+	CalendarID       string `env:"CALENDAR_ID"`
+	PlanningDaysSync int    `env:"PLANNING_DAYS_SYNC"`
+	Mode             string `env:"MODE"`
+}
 
-func LoadEnv() error {
+func LoadEnv() (*Config, error) {
+	var cfg = &Config{}
+
 	err := godotenv.Load(".env")
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	if err := env.Parse(&cfg); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func (c *Config) Validate() error {
+	// Validate the SchedulerCron field
+	if _, err := cron.ParseStandard(c.SchedulerCron); err != nil {
+		return fmt.Errorf("invalid SchedulerCron value: %w", err)
+	}
+
+	// Validate the PlanningDaysSync field
+	if c.PlanningDaysSync < 0 {
+		return fmt.Errorf("invalid PlanningDaysSync value: must be a positive integer")
+	}
+
 	return nil
 }
